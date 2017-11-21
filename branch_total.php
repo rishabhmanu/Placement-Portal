b880cdb6d9d3bc908dbb86f350ff2a4c6630d4d5
rishabhmanu <rishabhdevmanu@gmail.com>
2017-11-21 23:01:38 +0530

    student stats against branch

---
+++ branch_total.php
@@ -0,0 +1,44 @@
+ <?php  
+ $connect = mysqli_connect("localhost", "rishabh", "manu123", "placement");  
+ $query = " SELECT Branch, count(*) as number from student GROUP BY Branch";  
+ $result = mysqli_query($connect, $query);  
+ ?>  
+ <!DOCTYPE html>  
+ <html>  
+      <head>  
+           <title>Branch wise Division</title>  
+           <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>  
+             <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script type="text/javascript">
+           google.charts.load('current', {'packages':['corechart']});  
+           google.charts.setOnLoadCallback(drawChart);  
+           function drawChart()  
+           {  
+                var data = google.visualization.arrayToDataTable([  
+                          ['Branch', 'number'],  
+                          <?php  
+                          while($row = mysqli_fetch_array($result))  
+                          {  
+                               echo "['".$row["Branch"]."', ".$row["number"]."],";  
+                          }  
+                          ?>  
+                     ]);  
+                var options = {  
+                      title: 'Branch wise distribution of students',  
+                      //is3D:true,  
+                      pieHole: 0.4  
+                     };  
+                var chart = new google.visualization.PieChart(document.getElementById('piechart'));  
+                chart.draw(data, options);  
+           }  
+           </script>  
+      </head>  
+      <body>  
+           <br /><br />  
+           <div style="width:900px;">  
+                <h3 align="center">PIE CHART : Student distribution</h3>  
+                <br />  
+                <div id="piechart" style="width: 900px; height: 500px;"></div>  
+           </div>  
+      </body>  
+ </html>  
