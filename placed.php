20dfd585a93cf54adf2d409b9bc8e77753cbce7a
rishabhmanu <rishabhdevmanu@gmail.com>
2017-11-21 22:20:23 +0530

    Company-wise stats added

---
+++ placed.php
@@ -0,0 +1,44 @@
+ <?php  
+ $connect = mysqli_connect("localhost", "rishabh", "manu123", "placement");  
+ $query = "SELECT comp, count(*) as number from student WHERE comp IS NOT NULL GROUP BY comp";
+ $result = mysqli_query($connect, $query);  
+ ?>  
+ <!DOCTYPE html>  
+ <html>  
+      <head>  
+           <title>CTC Division</title>  
+           <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>  
+             <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script type="text/javascript">
+           google.charts.load('current', {'packages':['corechart']});  
+           google.charts.setOnLoadCallback(drawChart);  
+           function drawChart()  
+           {  
+                var data = google.visualization.arrayToDataTable([  
+                          ['comp', 'number'],  
+                          <?php  
+                          while($row = mysqli_fetch_array($result))  
+                          {  
+                               echo "['".$row["comp"]."', ".$row["number"]."],";  
+                          }  
+                          ?>  
+                     ]);  
+                var options = {  
+                      title: 'Company wise Statistics',  
+                      //is3D:true,  
+                      pieHole: 0.4  
+                     };  
+                var chart = new google.visualization.PieChart(document.getElementById('piechart'));  
+                chart.draw(data, options);  
+           }  
+           </script>  
+      </head>  
+      <body>  
+           <br /><br />  
+           <div style="width:900px;">  
+                <h3 align="center">PIE CHART : PLACEMENT DISTRIBUTION</h3>  
+                <br />  
+                <div id="piechart" style="width: 900px; height: 500px;"></div>  
+           </div>  
+      </body>  
+ </html>  
