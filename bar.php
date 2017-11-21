3122003b1ebcb943ee4cd40a023487c20bc9e90a
rishabhmanu <rishabhdevmanu@gmail.com>
2017-11-22 00:00:19 +0530

    dynamic graph of no. of companies visiting added

---
+++ bar.php
@@ -0,0 +1,51 @@
+ <?php  
+ $connect = mysqli_connect("localhost", "rishabh", "manu123", "placement");  
+ $query = " SELECT count(*) as number from company";  
+ $result = mysqli_query($connect, $query);  
+ ?>
+
+<!DOCTYPE HTML>
+<HEAD>
+	<TITLE>BAR GRAPH</TITLE>
+
+ <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+ <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+ <script type="text/javascript">
+  google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawBasic);
+
+function drawBasic() {
+
+      var data = google.visualization.arrayToDataTable([
+        ['Year', 'No. of companies visiting',],
+        ['2017-2018', <?php while($row = mysqli_fetch_array($result))  
+                          { echo $row["number"];} ?>],
+        // ['Los Angeles, CA', 3792000],
+        // ['Chicago, IL', 2695000],
+        // ['Houston, TX', 2099000],
+        // ['Philadelphia, PA', 1526000]
+      ]);
+
+      var options = {
+        // title: 'Population of Largest U.S. Cities',
+        chartArea: {width: '50%'},
+        hAxis: {
+          title: 'Total No. of Companies',
+          minValue: 0
+        },
+        vAxis: {
+          title: 'Academic Year'
+        }
+      };
+
+      var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+
+      chart.draw(data, options);
+    }
+</script>
+</HEAD>
+<BODY>
+	
+<div id="chart_div"></div>
+</BODY>
