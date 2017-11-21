40b989fd162ef60f32ce662085a5d323e3df242b
rishabhmanu <rishabhdevmanu@gmail.com>
2017-11-21 21:38:04 +0530

    generates statistics of CTC distribution

---
+++ ctc.php
@@ -0,0 +1,44 @@
+ <?php  
+ $connect = mysqli_connect("localhost", "rishabh", "manu123", "placement");  
+ $query = " SELECT CTC, count(*) as number from company, student where student.comp=company.id and student.comp IS NOT NULL GROUP BY CTC";  
+ $result = mysqli_query($connect, $query);  
+ ?>  
+ <!DOCTYPE html>  
+ <html>  
+      <head>  
+           <title>CTC Division</title>  
+           <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>  
+             <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script type="text/javascript">
+           google.charts.load('current', {'packages':['corechart']});  
+           google.charts.setOnLoadCallback(drawChart);  
+           function drawChart()  
+           {  
+                var data = google.visualization.arrayToDataTable([  
+                          ['CTC', 'number'],  
+                          <?php  
+                          while($row = mysqli_fetch_array($result))  
+                          {  
+                               echo "['".$row["CTC"]."', ".$row["number"]."],";  
+                          }  
+                          ?>  
+                     ]);  
+                var options = {  
+                      title: 'Distribution Of CTC among students placed(in lakhs)',  
+                      //is3D:true,  
+                      pieHole: 0.4  
+                     };  
+                var chart = new google.visualization.PieChart(document.getElementById('piechart'));  
+                chart.draw(data, options);  
+           }  
+           </script>  
+      </head>  
+      <body>  
+           <br /><br />  
+           <div style="width:900px;">  
+                <h3 align="center">PIE CHART : CTC distribution</h3>  
+                <br />  
+                <div id="piechart" style="width: 900px; height: 500px;"></div>  
+           </div>  
+      </body>  
+ </html>  
