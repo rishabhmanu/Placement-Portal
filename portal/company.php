acd7b804eaacd5cf38c3b4163e29c4e019425e0c
rishabhmanu <rishabhdevmanu@gmail.com>
2017-11-21 18:46:02 +0530

    Front-end updated

---
+++ portal/company.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Placement Portal | Recruiters</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="css/coin-slider.css" />
+<script type="text/javascript" src="js/cufon-yui.js"></script>
+<script type="text/javascript" src="js/droid_sans_400-droid_sans_700.font.js"></script>
+<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+<script type="text/javascript" src="js/coin-slider.min.js"></script>
+</head>
+<body>
+<div class="main">
+  <div class="header">
+    <div class="header_resize">
+      <div class="logo">
+        <h1><a href="index.html">Placement<span>Portal</span> <small>NIE Mysuru</small></a></h1>
+      </div>
+      <div class="clr"></div>
+      <div class="menu_nav">
+        <ul>
+          <li><a href="index.php"><span>Home Page</span></a></li>
+          <li><a href="student.php"><span>Student Corner</span></a></li>
+          <li class="active"><a href="company.php"><span>Companies/Recruiters</span></a></li>
+          <li><a href="about.html"><span>About Us</span></a></li>
+          <li><a href="contact.html"><span>Contact Us</span></a></li>
+        </ul>
+      </div>
+      <div class="clr"></div>
+      <div class="slider">
+        <div id="coin-slider"> <a href="#"><img src="images/slide1.jpg" width="960" height="335" alt="" /> </a> <a href="#"><img src="images/slide2.jpg" width="960" height="335" alt="" /> </a> <a href="#"><img src="images/slide3.jpg" width="960" height="335" alt="" /> </a> </div>
+        <div class="clr"></div>
+      </div>
+      <div class="clr"></div>
+    </div>
+  </div>
+  <div class="content">
+    <div class="content_resize">
+      <div class="mainbar">
+        <div class="article">
+          <h2><span>Companies/Recruiters</span></h2>
+          <div class="clr"></div>
+          <p><ul class="sb_menu">
+            <li><a href="\Placement_Portal\Company_register\index.html"><strong>New User</strong></a></li>
+            <li><a href="\Placement_Portal\Company_Login\index.php"><strong>Already Registered</strong></a></li>
+            
+          </ul> </p>
+        </div>
+        
+      </div>
+      <div class="sidebar">
+        <div class="gadget">
+          <h2 class="star"><span>Menu</span></h2>
+          <div class="clr"></div>
+          <ul class="sb_menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Student Corner</a></li>
+            <li><a href="#">Companies/Recruiters</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+        <div class="gadget">
+          <h2 class="star"><span>Contributors</span></h2>
+          <div class="clr"></div>
+          <ul class="ex_menu">
+            <li><a href="#">Rishabh Dev Manu</a><br />
+              4NI15CS077</li>
+            <li><a href="#">Rohit Kumar</a><br />
+              4NI15CS079</li>
+            <li><a href="#">Sanchit Snehashish</a><br />
+              4NI15CS086</li>
+            <li><a href="#">Saurabh Kumar Singh</a><br />
+              4NI15CS089</li>
+          </ul>
+        </div>
+      </div>
+      
+      <div class="clr"></div>
+    </div>
+  </div>
+  </div>
+  </div>
+  <div class="footer">
+    <div class="footer_resize">
+      <p class="lf">Copyright &copy; <a href="#">Placement Portal</a>. All Rights Reserved</p>
+      <p class="rf">Design by <a target="_blank" href="http://www.dreamtemplate.com/">DreamTemplate</a></p>
+      <div style="clear:both;"></div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
---
+++ portal/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>NIE Mysuru - Placements</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="css/coin-slider.css" />
+<script type="text/javascript" src="js/cufon-yui.js"></script>
+<script type="text/javascript" src="js/droid_sans_400-droid_sans_700.font.js"></script>
+<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+<script type="text/javascript" src="js/coin-slider.min.js"></script>
+</head>
+<body>
+<div class="main">
+  <div class="header">
+    <div class="header_resize">
+      <div class="logo">
+        <h1><a href="index.html">Placement<span>Portal</span> <small>NIE Mysuru</small></a></h1>
+      </div>
+      <div class="clr"></div>
+      <div class="menu_nav">
+        <ul>
+          <li class="active"><a href="index.php"><span>Home Page</span></a></li>
+          <li><a href="student.php"><span>Student Corner</span></a></li>
+          <li><a href="company.php"><span>Companies/Recruiters</span></a></li>
+          <!-- <li><a href="about.html"><span>About Us</span></a></li> -->
+          <!-- <li><a href="contact.html"><span>Contact Us</span></a></li> -->
+        </ul>
+      </div>
+      <div class="clr"></div>
+      <div class="slider">
+        <div id="coin-slider"> <a href="#"><img src="images/slide1.jpg" width="960" height="335" alt="" /> </a> <a href="#"><img src="images/slide2.jpg" width="960" height="335" alt="" /> </a> <a href="#"><img src="images/slide3.jpg" width="960" height="335" alt="" /> </a> </div>
+        <div class="clr"></div>
+      </div>
+      <div class="clr"></div>
+    </div>
+  </div>
+  <div class="content">
+    <div class="content_resize">
+      <div class="mainbar">
+        <div class="article">
+          <h2><span>Excellent Solution</span> For Your Placements</h2>
+          <p class="infopost">Posted on <span class="date">11 sep 2018</span> by <a href="#">Admin</a> &nbsp;&nbsp;|&nbsp;&nbsp; Filed under <a href="#">templates</a>, <a href="#">internet</a> <a href="#" class="com">Comments <span>11</span></a></p>
+          <div class="clr"></div>
+          <div class="img"><img src="images/img1.jpg" width="650" height="196" alt="" class="fl" /></div>
+          <div class="post_content">
+            <!-- content in p -->
+          </div>
+          <div class="clr"></div>
+        </div>
+        <!-- <p class="pages"><small>Page 1 of 2</small> <span>1</span> <a href="#">2</a> <a href="#">&raquo;</a></p> -->
+      </div>
+      <div class="sidebar">
+        <div class="gadget">
+          <h2 class="star"><span>Menu</span></h2>
+          <div class="clr"></div>
+          <ul class="sb_menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Student Corner</a></li>
+            <li><a href="#">Companies/Recruiters</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+        <div class="gadget">
+          <h2 class="star"><span>Contributors</span></h2>
+          <div class="clr"></div>
+          <ul class="ex_menu">
+            <li><a href="#">Rishabh Dev Manu</a><br />
+              4NI15CS077</li>
+            <li><a href="#">Rohit Kumar</a><br />
+              4NI15CS079</li>
+            <li><a href="#">Sanchit Snehashish</a><br />
+              4NI15CS086</li>
+            <li><a href="#">Saurabh Kumar Singh</a><br />
+              4NI15CS089</li>
+          </ul>
+        </div>
+      </div>
+      <div class="clr"></div>
+    </div>
+  </div>
+  </div>
+  <div class="footer">
+    <div class="footer_resize">
+      <p class="lf">Copyright &copy; <a href="#">Placement Portal</a>. All Rights Reserved</p>
+      <p class="rf">Design by <a target="_blank" href="http://www.dreamtemplate.com/">DreamTemplate</a></p>
+      <div style="clear:both;"></div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
